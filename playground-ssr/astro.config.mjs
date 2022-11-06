92bc4338fa3daca04d0b75148bee2c8d490ebb9b
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-11-06 19:05:22 +0100

    chore: add playground-ssr

---
+++ playground-ssr/astro.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: "W1vLyxT5rQ15jBpANjnv0gtt",
+      apiOptions: {
+        cache: {
+          clear: "auto",
+          type: "memory",
+        },
+      },
+      bridge: false,
+      ssr: true,
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+  output: "server",
+  adapter: netlify(),
+});
---
+++ playground-ssr/tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        primary: "#50b0ae",
+        secondary: "#1c243d",
+      },
+    },
+  },
+};
