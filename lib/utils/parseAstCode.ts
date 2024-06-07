788f318ede83ee26cd9331e96dc05bd8f117b377
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-06-07 20:19:09 +0530

    chore: type safe the last bits

--- lib/utils/parseAstCode.ts
+++ lib/utils/parseAstCode.ts
@@ -2,26 +2,33 @@ import mergeWith from "lodash.mergewith";
 import type { ISbStoriesParams, StoryblokBridgeConfigV2 } from "@storyblok/js";
 import type { RawCodeOptions } from "../vite-plugins/vite-plugin-storyblok-bridge";
 import type { Rollup } from "vite";
+import type { AwaitExpression, SpreadElement, Property } from "estree";
+
+/**
+ * Parses through the Abstract Syntax Tree (AST) code to locate the 'useStoryblok' function and its properties.
+ * This functionality is crucial for generating a virtual module that can be utilized during the initialization of the Storyblok bridge.
+ */
 
 export function parseAstRawCode(astCode: Rollup.ProgramNode) {
   let obj: RawCodeOptions = {};
 
-  function customizer(_: any, srcValue: any) {
+  function customizer(_: any, srcValue: AwaitExpression) {
     if (
       srcValue?.type === "AwaitExpression" &&
-      srcValue?.argument?.callee?.name === "useStoryblok"
+      srcValue.argument.type === "CallExpression" &&
+      srcValue.argument.callee.type === "Identifier" &&
+      srcValue.argument.callee.name === "useStoryblok"
     ) {
       const props = srcValue?.argument?.arguments;
       if (props && props[1]?.type === "ObjectExpression") {
-        const apiOptions = getAstPropToObj(props[1]?.properties || []);
+        const apiOptions = getAstPropToObj(props[1]?.properties);
         obj = {
           ...obj,
           apiOptions,
         };
       }
       if (props && props[2]?.type === "ObjectExpression") {
-        const bridgeOptions = getAstPropToObj(props[2]?.properties || []);
-
+        const bridgeOptions = getAstPropToObj(props[2]?.properties);
         obj = {
           ...obj,
           bridgeOptions,
@@ -34,24 +41,23 @@ export function parseAstRawCode(astCode: Rollup.ProgramNode) {
   return obj;
 }
 
-function getAstPropToObj(
-  properties: {
-    key: {
-      name: string;
-    };
-    value: any;
-  }[]
-) {
+function getAstPropToObj(properties: (SpreadElement | Property)[]) {
   const option: ISbStoriesParams | StoryblokBridgeConfigV2 = {};
 
-  return properties.reduce((options, { key, value }) => {
+  return properties.reduce((options, property) => {
+    if (property.type !== "Property") return options;
+    const { key, value } = property;
     const { type } = value;
-    options[key.name] =
-      type === "Literal"
-        ? value.value
-        : type === "ArrayExpression"
-          ? value.elements.map((v) => v.value)
-          : value.value;
+    if (key.type !== "Identifier") return options;
+
+    if (type === "Literal") {
+      options[key.name] = value.value;
+    } else if (type === "ArrayExpression") {
+      options[key.name] = value.elements
+        .filter((v) => v.type === "Literal")
+        .map((v) => v.value);
+    }
+
     return options;
   }, option);
 }
