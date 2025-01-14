335c7b2204f6edeba33a4f808e64b1b7ad7abef7
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2025-01-14 18:16:57 +0530

    chore: cleanup index.ts file

--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,7 @@ export type {
   ISbStory, // previously Story
   ISbStoryData, // previously StoryData
   ISbStoryParams, // previously StoryParams
+  RichTextResolver,
   SbBlokData,
   SbBlokKeyDataTypes,
   SbPluginFactory,
